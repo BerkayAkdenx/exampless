bf50fe4b6b18d9cc80e1f71b2a4817572485c94d
İsmet Berkay Akdeniz <74598142+BerkayAkdenx@users.noreply.github.com>
2023-03-29 20:02:24 +0300

    welcome view is created
    project of quiz work

---
+++ quiz_work/lib/views/welcome.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_work/constants.dart';
+
+class WelcomeView extends StatelessWidget {
+  const WelcomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/bg.png"),
+                  fit: BoxFit.cover)),
+        ),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Spacer(
+                  flex: 2,
+                ),
+                Text(
+                  "Lets Play Quiz",
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const Text("Enter your informations below"),
+                const Spacer(),
+                const TextField(
+                  decoration: InputDecoration(
+                    hintText: "Full Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                  ),
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(kDefaultPadding * 0.75),
+                    decoration: const BoxDecoration(
+                      gradient: kPrimaryGradient,
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                    child: Text(
+                      "Lets Start Quiz",
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelLarge!
+                          .copyWith(color: Colors.black),
+                    ),
+                  ),
+                ),
+                const Spacer(
+                  flex: 2,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
